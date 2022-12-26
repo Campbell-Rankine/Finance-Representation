07dcf3fd408c6a0e6de6123b6ecc9cf6715aa33b
Campbell-Rankine <campbellrankine@gmail.com>
2022-12-26 11:31:52 -0500

    Rewards, pricings, etc. Still more to be done

--- env.py
+++ env.py
@@ -18,19 +18,22 @@ import tensorboard
 from torch.utils.tensorboard import SummaryWriter
 
 class PreTrainEnv(gym.env):
-    def __init__(self, initial_fund, trade_price, dims, num_tickers, max_hold, max_stock_value, window, _iters_):
+    def __init__(self, initial_fund, trade_price, dims, num_tickers, max_hold, max_stock_value, window, _iters_, dataset,
+                tolerance):
         super(PreTrainEnv, self).__init__()
 
         ### - Reset Vars - ###
         self.initial = initial_fund
         self.trade_price = trade_price
-        self.data = T.zeros_like(dims)
+        self.data = dataset #(DATASET FORMAT, self.dims = (num_tickers, num_features, num_samples))
+
         self.time_init = 0
         self.num_tickers = num_tickers
 
         ### - Shape - ###
         self.dims = dims
         self.window = window
+        assert(self.window < (self.dims[-1]/6))
 
         ### - Environment Attributes - ###
         self.timestep = 0
@@ -38,6 +41,7 @@ class PreTrainEnv(gym.env):
         self.available_funds = self.initial
         self.max_hold = max_hold
         self.max_price = max_stock_value
+        self.tolerance = tolerance
 
         ### - Spaces - ###
         self.action_space = self.gen_action_space() 
@@ -52,7 +56,7 @@ class PreTrainEnv(gym.env):
         Agent has control over integer counts of each stock. At each timestep during forward testing amounts will be
         tracked and differences in amounts at time T will mean buy/sell/hold commands of that difference
         """
-        return spaces.Box(low=np.zeros(self.num_tickers), high=self.max_hold*np.ones(self.num_tickers), dtype=np.int32)
+        return spaces.Box(low=np.zeros(self.num_tickers), high=np.ones(self.num_tickers), dtype=np.int32)
     
     def gen_obs_space(self):
         """
@@ -61,15 +65,12 @@ class PreTrainEnv(gym.env):
         return spaces.Box(low=np.zeros_like(self.dims), high=self.max_price*np.ones_like(self.dims), dtype=np.float32)
     
     def _get_observation(self):
-        #TODO: Implement observation retrieval from dataset.
-        raise NotImplementedError
-    
-    def _get_action(self):
-        #TODO: Implement next action retrieval
-        raise NotImplementedError
+        return self.data[:][:][self.timestep, max(self.timestep+self.window, self.dims[-1])]
     
     def _get_current_prices(self):
-        #TODO: Get current timestep stock prices
+        """
+        For the sake of adding noise sample a random price from somewhere within the [t - tolerance, t + tolerance] range
+        """
         raise NotImplementedError
 
     def _reward(self):
@@ -78,9 +79,9 @@ class PreTrainEnv(gym.env):
         time_pen = self.timestep / self.MAX_ITERS
         return curr_worth * time_pen
 
-    def step(self):
+    def step(self, action):
         #update for reward
-        self.holdings = self._get_action()
+        self.holdings = action
         self.current_prices = self._get_current_prices()
         self.timestep += 1
 
@@ -94,6 +95,18 @@ class PreTrainEnv(gym.env):
         return obs, worth, done, {}
 
 
+    def reset(self):
+        ### - Resets - ###
+        self.holdings = np.zeros(self.num_tickers)
+        self.current_prices = np.zeros(self.num_tickers)
+        self.available_funds = self.initial
+        self.timestep = self.time_init #TODO: add random timestep init
+        self.data = np.zeros_like(self.dims)
 
+        ### - first obs - ###
+        obs = self._get_observation()
+        return obs
 
-
+    def render(self):
+        #TODO: save price graphs for each stock to a folder, and current holdings will be included on graph
+        raise NotImplementedError
