17561831d7e995d9a9a9a3df2afb2f90b3fa855b
Campbell-Rankine <campbellrankine@gmail.com>
2022-12-27 09:19:28 -0500

    Replay buffer: continuous DDPG

---
+++ Trader/buffer.py
@@ -0,0 +1,38 @@
+from collections import deque
+import random
+
+class ReplayBuffer(object):
+    """
+    Continuous DDPG replay memory buffer for storing state tuples to sample
+    """
+    def __init__(self, buffer_size):
+        self.size = buffer_size
+        self.storage = deque()
+        self.num_exp = 0
+
+    def add(self, state, action, reward, new_state, done):
+        if self.available():
+            self.storage.append((state, action, reward, new_state, done))
+            self.num_exp += 1
+        else:
+            self.storage.popleft()
+            self.storage.append((state, action, reward, new_state, done))
+    
+    def sample(self, sample_size):
+        if self.num_experiences < sample_size:
+            return random.sample(self.buffer, self.num_experiences)
+        else:
+            return random.sample(self.buffer, sample_size)
+
+    
+    ### - ATTRIBUTE FUNCTIONS - ###
+
+    def size(self):
+        return self.size
+
+    def available(self):
+        """
+        Boolean function, finds if memory has space for an additional state
+        """
+        return (self.size - (self.num_exp + 1)) <= 0
+    
